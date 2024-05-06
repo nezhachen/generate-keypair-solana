09f29ddcda128358618872061c9b38848b169731
nezhachen <sgp.chenwei614@gmail.com>
2024-05-06 15:32:52 +0200

    Add the getBalance feature

--- example.ts
+++ example.ts
@@ -1,4 +1,8 @@
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 
 const connection = new Connection(clusterApiUrl("devnet"));
-console.log(`✅ Connected!`)
+const address = new PublicKey('HqoEqtrvWZwHj5VBFs77XMuhg18Ba899mutnkCvwMPne');
+const balance = await connection.getBalance(address);
+
+console.log(`The balance of the account at ${address} is ${balance} lamports`); 
+console.log(`✅ Finished!`)
